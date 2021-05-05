75d5c0700333596428792fa04013e5ebe82eeb5a
tejaswinikandra111 <80246832+tejaswinikandra111@users.noreply.github.com>
2021-05-05 11:51:22 +0530

    Update DemoApplicationTests.java

--- demo/src/test/java/com/mindtree/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/mindtree/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mindtree.demo;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
